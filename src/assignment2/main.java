8c3c0791b2c6dcef2ae914fb259ae3c4813c354b
jennifer <jpyon@student.ubc.ca>
2025-07-25 15:18:44 -0700

    first commit

---
+++ src/assignment2/main.java
@@ -0,0 +1,38 @@
+// Jennifer Pyon, bmeg coding intensive assignment 2
+
+package assignment2;
+
+class main {
+
+    public static void main (String[] args) {
+
+        //Changing (refactor) the variable name "foo" to "NewFoo"
+        int[] NewFoo = {0, 1, 2, 3, 4};
+
+        int[] bar = new int[100];
+        for(int i = 0; i < 100; i++){
+            bar[i] = i;
+        }
+
+        System.out.println("The number of even numbers in NewFoo is:");
+        System.out.println(countEvenNum(NewFoo, 5));
+
+        System.out.println("The number of even numbers in bar is:");
+        System.out.println(countEvenNum(bar, 100));
+
+    }
+
+
+    private static int countEvenNum(int[] arr, int size){
+
+        int count = 0;
+
+        for(int i = 0; i < size; i++) {
+            if(arr[i] % 2 == 0){
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
